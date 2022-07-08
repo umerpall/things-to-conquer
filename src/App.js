eda1b32a74613182864e71254f64055a44f27b44
Umer <umerpall124@gmail.com>
2022-07-08 09:05:19 +0500

    First Client Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [todoItems, setTodoItems] = useState([]);
+  const [inputVal, setInputVal] = useState('');
+  const [isUpdating, setIsUpdating] = useState('');
+  const [updateItemText, setUpdateItemText] = useState('');
+
+  useEffect(()=>{
+    const getItems = async () => {
+      const todos = await axios.get('http://localhost:5500/items')
+      setTodoItems(todos.data);
+    }
+    getItems();
+  }, [])
+
+
+  const addItems = async () => {
+    const todos = await axios.post('http://localhost:5500/items',
+      {
+        item: inputVal
+      }
+    )
+    setTodoItems((prevTodos)=>([...prevTodos, todos.data]))
+    setInputVal('');
+  }
+
+  const deleteItem = async(id) => {
+    await axios.delete(`http://localhost:5500/items/${id}`)
+    const newListItems = todoItems.filter((todo)=>(todo._id !== id))
+    // console.log(newListItems)
+    setTodoItems(newListItems)
+  }
+
+  const updateItem = async() => {
+    await axios.put('http://localhost:5500/items/' + isUpdating, {item:updateItemText})
+    const updatedItemID= todoItems.findIndex((item)=>item._id === isUpdating)
+    todoItems[updatedItemID].item = updateItemText
+    setIsUpdating('')
+    setUpdateItemText('')
+  }
+
+  const renderForm = () => (
+    <>
+      <input class='update-input' type="text" value={updateItemText} placeholder="Add Updated Item" onChange={(e)=>setUpdateItemText(e.target.value)}/>
+      <button class='update-btn' onClick={()=>updateItem()}>Update</button>
+    </>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div className="container">
+        <div className="input-part">
+          <input type="text" placeholder="Add an Item" value={inputVal} onChange={(e)=>setInputVal(e.target.value)} />
+          <button onClick={()=>addItems()}>Add</button>
+        </div>
+        <div className="items-list">
+          {todoItems.map((todo)=>(
+            <div className="item" key={todo._id}>
+              {
+                isUpdating === todo._id
+                ? renderForm()
+                : <>
+                    <p>{todo.item}</p>
+                    <div>
+                      <button onClick={()=>setIsUpdating(todo._id)}>Update</button>
+                      <button onClick={()=>deleteItem(todo._id)}>Delete</button>
+                    </div>
+                  </>
+              }
+          </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
